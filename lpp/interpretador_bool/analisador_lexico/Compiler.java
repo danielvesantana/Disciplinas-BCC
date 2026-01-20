3f4569c68ed7f3f925b547a083590dab21527f8a
Daniel Alves <danielvesantana@gmail.com>
2026-01-20 13:35:32 -0300

    compiler

--- lpp/interpretador_bool/analisador_lexico/Compiler.java
+++ lpp/interpretador_bool/analisador_lexico/Compiler.java
@@ -21,24 +21,24 @@ public class Compiler {
         rules.add(new Rule("^[a-zA-Z]+\\s=\\s[0-9]+")); // var = number
         rules.add(new Rule("^[a-zA-Z]+\\s=\\s[a-zA-Z]+")); // var = var
         rules.add(new Rule("^[a-zA-Z]+\\s=\\s[a-zA-Z]+\\.[a-zA-Z]+")); // var = obj.atr 
-        rules.add(new Rule("^[a-zA-Z]+\\s=\\s[a-zA-Z]+\\\\.[a-zA-Z]+\\(\\)")); //var = obj.method()
-        rules.add(new Rule("^[a-zA-Z]+\\s=\\s[a-zA-Z]+\\\\.[a-zA-Z]+\\([a-zA-Z]+(,\\s[a-zA-Z])*\\)")); //var = obj.method(var1, var2, ..., varN)
+        rules.add(new Rule("^[a-zA-Z]+\\s=\\s[a-zA-Z]+\\.[a-zA-Z]+\\(\\)")); //var = obj.method()
+        rules.add(new Rule("^[a-zA-Z]+\\s=\\s[a-zA-Z]+\\.[a-zA-Z]+\\([a-zA-Z]+(,\\s[a-zA-Z])*\\)")); //var = obj.method(var1, var2, ..., varN)
+        rules.add(new Rule("^[a-zA-Z]+\\s=\\snew\\s[a-zA-Z]+")); //var = new Obj
+        rules.add(new Rule("^[a-zA-Z]+\\s=\\s[a-zA-Z]+\\s[+-*/]\\s[a-zA-Z]+")); //var = var [+-*/] var
         rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\s=\\s[0-9]+")); //obj.atr = number
         rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\s=\\s[a-zA-Z]+")); // obj.atr = var
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
-        rules.add(new Rule(""));
+        rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\s=\\s[a-zA-Z]+\\.[a-zA-Z]+")); // obj.atr = obj.atr
+        rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\s=\\s[a-zA-Z]+\\.[a-zA-Z]+\\(\\)")); // obj.atr = obj.method()
+        rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\s=\\s[a-zA-Z]+\\.[a-zA-Z]+\\([a-zA-Z]+(,\\s[a-zA-Z])*\\)")); // obj.atr = obj.method(var1, var2, ..., varN)
+        rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\s=\\snew\\s[a-zA-Z]+")); // obj.atr = new Obj
+        rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\s=\\s[a-zA-Z]+\\s[+-*/]\\s[a-zA-Z]+")); // obj.atr = var [+-*/] var
+        rules.add(new Rule("^if\\s[a-zA-Z]+\\s(eq|ne|gt|ge|lt|le)\\s[a-zA-Z]+\\sthen")); // if var (eq|ne|gt|ge|lt|le) var then
+        rules.add(new Rule("^else")); //else
+        rules.add(new Rule("^return\\s[a-zA-Z]+")); // return var
+        rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\(\\)")); // obj.method()
+        rules.add(new Rule("^[a-zA-Z]+\\.[a-zA-Z]+\\([a-zA-Z]+(,\\s[a-zA-Z])*\\)")); // obj.method(var1, var2, ..., varN)
+        rules.add(new Rule("^[a-zA-Z]+\\._prototype\\s=\\s[a-zA-Z]+")); // var._prototype = var
+        rules.add(new Rule("^new\\s[a-zA-Z]+")); // new Obj
     }
 
     public void processRules(){
