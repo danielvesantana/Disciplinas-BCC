14ea19d8b677e4df8a3ccde6ba22b04c6ae7e9a0
Daniel Alves <danielvesantana@gmail.com>
2026-03-12 22:45:08 -0300

    bnf rules

--- lpp/interpretador_bool/syntactic_analysis/BNFRules.java
+++ lpp/interpretador_bool/syntactic_analysis/BNFRules.java
@@ -1,61 +1,77 @@
 package syntactic_analysis;
 
+import java.util.LinkedList;
 import java.util.List;
 import lexical_analysis.Token;
 
 public class BNFRules {
-
-    public void classBNF(List<Token> tokens, int index) {
+    public List<Token> classBNF(List<Token> tokens, int index) {
     }
 
-    public void methodBNF(List<Token> tokens, int index) {
+    public List<Token> methodBNF(List<Token> tokens, int index) {
     }
     
-    public void beginBNF(List<Token> tokens, int index) {
+    public List<Token> beginBNF(List<Token> tokens, int index) {
     }
     
-    public void selfBNF(List<Token> tokens, int index) {
+    public List<Token> selfBNF(List<Token> tokens, int index) {
     }
 
-    public void varsBNF(List<Token> tokens, int index) {
+    public List<Token> varsBNF(List<Token> tokens, int index) {
+        List<Token> bnfList = new LinkedList<>();
+        bnfList.add(tokens.get(index));
+        
+        for(int i = index; i < tokens.size(); i++){
+            if(tokens.get(i) == Token.NAME && 
+               tokens.get(i+1) == Token.COMMA && 
+               tokens.get(i+2) == Token.NAME){
+                bnfList.add(tokens.get(i));
+                bnfList.add(tokens.get(i+1));
+                bnfList.add(tokens.get(i+2));
+            }else if(tokens.get(i) == Token.NAME){
+                bnfList.add(tokens.get(i));
+            }
+        }
+
+        return bnfList;
     }
 
-    public void ifBNF(List<Token> tokens, int index) {
+    public List<Token> ifBNF(List<Token> tokens, int index) {
     }
 
-    public void elseBNF(List<Token> tokens, int index) {
+    public List<Token> elseBNF(List<Token> tokens, int index) {
     }
 
-    public void returnBNF(List<Token> tokens, int index) {
+    public List<Token> returnBNF(List<Token> tokens, int index) {
     }
 
-    public void newBNF(List<Token> tokens, int index) {
+    public List<Token> newBNF(List<Token> tokens, int index) {
     }
 
-    public void mainBNF(List<Token> tokens, int index) {
+    public List<Token> mainBNF(List<Token> tokens, int index) {
     }
 
-    public void ioBNF(List<Token> tokens, int index) {
+    public List<Token> ioBNF(List<Token> tokens, int index) {
     }
 
-    public void nameBNF(List<Token> tokens, int index) {
+    public List<Token> nameBNF(List<Token> tokens, int index) {
     }
 
-    public void lparenBNF(List<Token> tokens, int index) {
+    public List<Token> lparenBNF(List<Token> tokens, int index) {
     }
 
-    public void rparenBNF(List<Token> tokens, int index) {
+    public List<Token> rparenBNF(List<Token> tokens, int index) {
     }
 
-    public void dotBNF(List<Token> tokens, int index) {
+    public List<Token> dotBNF(List<Token> tokens, int index) {
     }
 
-    public void commaBNF(List<Token> tokens, int index) {
+    public List<Token> commaBNF(List<Token> tokens, int index) {
     }
 
-    public void prototypeBNF(List<Token> tokens, int index) {
+    public List<Token> prototypeBNF(List<Token> tokens, int index) {
     }
 
-    public void methodNameBNF(List<Token> tokens, int index) {
+    public List<Token> methodNameBNF(List<Token> tokens, int index) {
     }
 }
